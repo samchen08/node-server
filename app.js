f922f1439b10ac3c490b6feece3ed8553b8363ad
samchen08 <samchen08@163.com>
2016-07-27 20:54:06 +0800

    fix app cluster bug

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var cluster = require('cluster');
 var cups = require('os').cpus();
 
 var server = require('./server');
+
+//获取logger对象, 日志用console的名称分组
 var logger = require('./logger').logger('console');
 
 if (cluster.isMaster) {
     var workers = [];
+
+    //创建子进程,并且当子进程退出的时候自动恢复
     var spawn = function (i) {
         workers[i] = cluster.fork();
         workers[i].on('exit', function (worker, ocde, signal) {
@@ -15,18 +19,18 @@ if (cluster.isMaster) {
         });
     };
 
-    cups.forEach(function () {
-        cluster.fork();
-    });
-
+    //子进程创建完成后, 提示信息
     cluster.on('listening', function (worker, server) {
         logger.info('worker connected to', server.address, server.port, worker.id);
     });
 
+    //根据cpu内核个数创建子进程
+    cups.forEach(function (n, i) {
+        spawn(i)
+    });
+    
     logger.info('server running....');
 
 } else {
-
     server.init();
-
 }
